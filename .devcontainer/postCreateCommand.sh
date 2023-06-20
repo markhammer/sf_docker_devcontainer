21a69fa17cc111afcdbadf37d5265ff9d2e184e5
Mark Hammer <37933492+markhammer@users.noreply.github.com>
2023-06-20 15:45:04 +0900

    add plugins

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,5 +2,5 @@ apt-get update -y
 sleep 5
 apt-get install -y g++ build-essential
 sleep 5
-sf plugins install @salesforce/lwc-dev-server
+sf plugins install @salesforce/lwc-dev-server @salesforce/plugin-packaging
 if [ ! -e './sfdx-project.json' ]; then sf project generate --name $(basename `pwd`) -x -d ../ && echo .devcontainer/ >> .gitignore; fi
